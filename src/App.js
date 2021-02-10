8cc52898707f4e743ba787a33859533e69345670
lucasjesusone <lucasjesusone@gmail.com>
2021-02-09 23:47:29 -0400

    #5 Building Form

--- src/App.js
+++ src/App.js
@@ -3,15 +3,17 @@ import Header from './components/header/Header'
 import About from './components/about/About'
 import Main from './components/main/Main'
 import Mission from './components/mission/Mission'
+import Contact from './components/contact/Contact'
 export default class App extends Component {
  render(){
   return (
-    <div>
+    <div className='container'>
 
       <Header/>
       <About/>
       <Main/>
       <Mission/>
+      <Contact/>
     </div>
    );
  }
---
+++ src/components/form/Form.js
@@ -0,0 +1,119 @@
+import React, { Component } from 'react';
+import '../form/form.css'
+
+export default class Form extends Component {
+ constructor(){
+     super();
+     this.state = {
+        name: '',
+        email: '',
+        message: '',
+        formError: false
+        }
+    }    
+
+
+    getName = (event) => {
+        let username = event.target.value;
+        this.setState({
+            name: username
+        });
+
+        console.log(this.state.name)
+    } 
+
+    getEmail = (event) => {
+        let userEmail = event.target.value;
+        //the most important thing is that we use a RegEx
+        //in order to manage the input of the email
+        //at least we can get a some what valid email
+        if(userEmail.match(/^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$/)){ 
+            this.setState({
+                email: userEmail
+        });
+            }else {
+            this.setState({
+                email: ''
+            });
+            console.log('Incorrect Email, Must match Expression')
+        };
+
+        console.log(this.state.email)
+    }
+
+
+    getDescription = (event) => {
+        let userMessage = event.target.value;
+        this.setState({
+            message: userMessage
+        });
+        console.log(this.state.message)
+    }
+
+
+    //sending form
+
+    submitForm = (event) =>{
+        event.preventDefault();
+
+
+
+        if(this.state.name === '' || this.state.email === '' || this.state.message === '') {
+            this.setState({
+                formError: true
+            })
+            return false;
+        } else {
+           
+           
+           
+            this.setState({
+                formError: false
+             })
+            console.log(`UserData: {
+                Username: ${this.state.name},
+                Email: ${this.state.email},
+                Message: ${this.state.message}
+            }`)
+        
+        
+            console.log("form sent")
+        
+        }
+     
+      }
+    render() {
+
+
+        return (
+            <form>
+            {/* I am just sending a basic error message */}
+                {this.state.formError && 
+                <p className='error'>
+                    Preencha todos os campos
+                </p>
+                }
+                <p>Preencha o seguinte formulário para entrar em contato conosco</p>
+                <div>
+                    <label htmlFor='name'>Nome</label>
+                    <input type='text' name='name' placeholder='Seu nome' onChange={this.getName}/>
+                </div>
+                <div>
+                    <label htmlFor='email'>Email</label>
+                    <input type='email' name='email' placeholder='Seu Email' onChange={this.getEmail} />
+                </div>
+                <div >
+                    <label htmlFor='name'>Message</label>
+                    <textarea onChange={this.getDescription} maxLength='450'></textarea>
+                </div>
+                
+                <div>
+                    <p>We'll Answer as soon as possible</p>
+                    <input type='submit' name='submit' value='Send' onClick={this.submitForm}/>                
+                </div>
+
+
+            </form>
+        )
+    }
+}
