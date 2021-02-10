94eea16e71570f2ee1a07fcf06df59b79ab9f041
lucasjesusone <lucasjesusone@gmail.com>
2021-02-09 21:43:06 -0400

    #3 Building - Main

--- src/components/main/Main.js
+++ src/components/main/Main.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import '../main/main.css'
-import Icon from 'react-native-vector-icons/'
+import { FiCheck, FiTruck } from 'react-icons/fi'
+import {BiCodeAlt, BiBookBookmark, BiBarChartAlt2} from 'react-icons/bi'
+import { GiSandsOfTime } from 'react-icons/gi'
 
 
 export default class Main extends Component {
@@ -12,42 +14,42 @@ export default class Main extends Component {
             <div className='services'>
             {/*SERVIÇO UM -- PLANEJAMENTO*/}
             <div className='service-one'>
-            <p className='service-icon'><i className='far fa-calendar-alt'></i></p>
+            <p className='service-icon'><FiCheck/></p>
             <p className='service-title'>Planejamento</p>
             <p>LOREM IPSUS</p>
             </div>
             
              {/*SERVIÇO DOIS -- PLANEJAMENTO*/}
             <div className='service-two'>
-            <p className='service-icon'><i className='far fa-calendar-alt'></i></p>
+            <p className='service-icon'><BiCodeAlt/></p>
             <p className='service-title'>Planejamento</p>
             <p>LOREM IPSUS</p>
             </div>
 
             {/*SERVIÇO TRÊS -- PLANEJAMENTO*/}
             <div className='service-three'>
-            <p className='service-icon'><i className='far fa-calendar-alt'></i></p>
+            <p className='service-icon'><FiTruck/></p>
             <p className='service-title'>Planejamento</p>
             <p>LOREM IPSUS</p>
             </div>
 
             {/*SERVIÇO TRÊS -- PLANEJAMENTO*/}
             <div className='service-four'>
-            <p className='service-icon'><i className='far fa-calendar-alt'></i></p>
+            <p className='service-icon'><GiSandsOfTime/></p>
             <p className='service-title'>Planejamento</p>
             <p>LOREM IPSUS</p>
             </div>
 
             {/*SERVIÇO TRÊS -- PLANEJAMENTO*/}
             <div className='service-five'>
-            <p className='service-icon'><i className='far fa-calendar-alt'></i></p>
+            <p className='service-icon'><BiBarChartAlt2/></p>
             <p className='service-title'>Planejamento</p>
             <p>LOREM IPSUS</p>
             </div>
 
             {/*SERVIÇO TRÊS -- PLANEJAMENTO*/}
             <div className='service-six'>
-            <p className='service-icon'><i className='far fa-calendar-alt'></i></p>
+            <p className='service-icon'><BiBookBookmark/></p>
             <p className='service-title'>Planejamento</p>
             <p>LOREM IPSUS</p>
             </div>
---
+++ src/components/mission/Mission.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+
+
+export default class Mission extends Component {
+ render(){
+  return (
+    
+   );
+ }
+}
