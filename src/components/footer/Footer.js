0054d4ab31042f1fb8ea3551e977472a4c54f37a
lucasjesusone <lucasjesusone@gmail.com>
2021-02-14 23:19:21 -0400

    #31 Icon Footer Animation

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -11,8 +11,8 @@ export default class Footer extends Component {
         <h3></h3>
         <p>Support<br/> Lucasjesusone@gmail.com</p>
         <ul>
-            <li><a href="https://www.linkedin.com/in/lucasgabrieljesus/" target="_blank"><FaLinkedinIn/></a></li>
-            <li><a href="https://github.com/LucasJesusone" target="_blank"><AiFillGithub/></a></li>
+            <li><a href="https://www.linkedin.com/in/lucasgabrieljesus/" target="_blank"><FaLinkedinIn className='icon'/></a></li>
+            <li><a href="https://github.com/LucasJesusone" target="_blank"><AiFillGithub className='icon'/></a></li>
         </ul>
     </footer>
    );
