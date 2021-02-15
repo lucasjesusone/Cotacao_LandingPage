575f0b877f0af4ee06a42feede85a05360040741
lucasjesusone <lucasjesusone@gmail.com>
2021-02-14 22:22:16 -0400

    #23 Cleaning Code

---
+++ src/components/nav/MenuItems.js
@@ -0,0 +1,27 @@
+export const MenuItems = [
+    {
+        title: 'Sobre',
+        url: '#',
+        cName: 'nav-links' //className
+    },
+
+    {
+        title: 'Missão',
+        url: '#',
+        cName: 'nav-links' //className
+    },
+
+    {
+        title: 'Contato',
+        url: '#',
+        cName: 'nav-links' //className
+    },
+
+    {
+        title: 'Sign up',
+        url: '#',
+        cName: 'nav-links-mobile'
+    }
+
+    
+]
--- src/components/nav/Nav.js
+++ src/components/nav/Nav.js
@@ -1,24 +1,43 @@
 import React, { Component } from 'react';
+
 import '../../components/nav/nav.css'
+import { Button } from './Button'
+import { MenuItems } from '../nav/MenuItems'
+
+
+
 
 export default class Nav extends Component {
- render(){
-  return (
-    <nav>
-           
-            <ul>
-                <li className='logo'> <span> COTAR.ME</span></li>
-            </ul>
-        
-            
-            
-            <ul>
-                <li><a href='#mission'>Contatos</a></li>
-                <li><a href='contact'>Mission</a></li>
-                <li><a href='about-us'>Login</a></li>
-            </ul>
-        
+    state = {clicked: false}
+ 
 
+    onhandleClick = () => {
+        this.setState({ clicked: !this.state.clicked })
+    }
+  render(){
+  return (
+    <nav className='navbar'>
+            
+           <h1 className='navbar-logo'>COTAR.ME</h1>
+           
+           
+           <div className='menu-icon' onClick={this.onhandleClick}>
+              <i className={this.state.clicked ? 'fas fa-times' : 'fas fa-bars'}></i>
+            </div>
+
+            <ul className={this.state.clicked ? 'nav-menu active' : 'nav-menu'}>
+                {MenuItems.map((item, index) => {
+                  return (
+                    <li key={index}>
+                      <a className={item.cName} href={item.url}>
+                        {item.title}
+                      </a>
+                    </li>
+                  )
+                })}
+                <Button>Sign up</Button>
+            </ul>
+            
     </nav>
    );
  }
