9881bde91c0965c52cfcba042cffff30c110c291
lucasjesusone <lucasjesusone@gmail.com>
2021-02-18 01:04:41 -0400

    #61 ...Cleaning Code

--- src/components/mission/Mission.jsx
+++ src/components/mission/Mission.jsx
@@ -1,4 +1,4 @@
-import React, { Component, useContext } from 'react';
+import React, { useContext } from 'react';
 import LandingProvider from '../../context/context';
 import '../mission/mission.css'
 
@@ -7,18 +7,18 @@ const Mission = () => {
     const { title, paragraph1, paragraph2} = mission
 
   return (
-    <div id='mission'>
+    <div className='divMission' id='mission'>
         <section className='sectionMission'>
             <h2 className='title'>{title || 'Something'}</h2>
             <div>
-                     <a>
+                     <div >
                            <p className='paragraph'>
                                {paragraph1 || 'something'}
                            </p>
-                           <p className='paragraph'>
+                             <p className='paragraph'>
                                {paragraph2 || 'something'}
                            </p>
-                    </a>
+                 </div>
             </div>
         </section>
     </div>
