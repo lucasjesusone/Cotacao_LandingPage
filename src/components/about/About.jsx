25a2af399cf13f463c1c676c402e714df9e19f82
lucasjesusone <lucasjesusone@gmail.com>
2021-02-20 00:31:42 -0400

    #98 UseState and UseEffect

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,30 +1,41 @@
-import React, { useContext } from 'react';
-import LandingContext from '../../context/context';
-import '../about/about.css'
-
+import React, { useContext, useState, useEffect } from "react";
+import Fade from "react-reveal/Fade";
+import LandingContext from "../../context/context";
+import "../about/about.css";
 
 const About = () => {
-const { about } = useContext(LandingContext);
-const { textabout, message } = about;
-  
-    return ( 
-        <div id='about'>
-            <div className='about-us' >
-                <section className='intro'>
-                    <h2 className='title'>{textabout || 'Lorem'}</h2>
-                         <div>
-             
-                                <div>
-                                     <p className='textMessage'>
-                                         {message || 'Lorem'}
-                                     </p>
-                    </div>
-             </div>
-        </section>
-    </div>
-</div>
-   );
- }
+  const { about } = useContext(LandingContext);
+  const { textabout, message } = about;
 
+  const [isDesktop, setIsDesktop] = useState(false);
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    if (window.innerWidth > 769) {
+      setIsDesktop(true);
+      setIsMobile(false);
+    } else {
+      setIsMobile(true);
+      setIsDesktop(false);
+    }
+  }, []);
+
+  return (
+    <div id="about">
+      <div className="about-us">
+        <section className="intro">
+          <Fade left={isDesktop} bottom={isMobile} duration={1000} delay={600}>
+            <h2 className="title">{textabout || "Lorem"}</h2>
+          </Fade>
+          <Fade right={isDesktop} bottom={isMobile} duration={1000} delay={600}>
+            <div>
+              <p className="textMessage">{message || "Lorem"}</p>
+            </div>
+          </Fade>
+        </section>
+      </div>
+    </div>
+  );
+};
 
 export default About;
