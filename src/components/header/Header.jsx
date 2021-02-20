1c4b7a31ed0cfc46710fad0657199447e8dd5b3e
lucasjesusone <lucasjesusone@gmail.com>
2021-02-20 00:31:07 -0400

    #94 UseState and UseEffect

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,31 +1,66 @@
-  import React, { useContext } from 'react';
-  import { Link } from 'react-scroll'
-  import LandingProvider from '../../context/context'
-  import '../header/header.css'
-  
+import React, { useContext, useState, useEffect } from "react";
+import { Link } from "react-scroll";
+import Fade from "react-reveal/Fade";
+import LandingProvider from "../../context/context";
+import "../header/header.css";
 
 const Header = () => {
-const { header } = useContext(LandingProvider)
-const { title, url, text, button } = header
-  
-    return (
-      <header className='header' id='mapHeader'>
-          
-          <div className='head'>
-              <h1 className='title-header'>{title}<br/>CHANGING THE WORLD</h1>
-                <div>
-                    <div>
-                       <p className='message' >
-                           {text}
-                       </p>
-                      <Link to='contact' smooth duration={1000} className='contact' href={url}>
-                        {button  || 'Contact us'}
-                      </Link>
-                </div>
-              </div>
-          </div>
-      </header>
-    );
-  }
+  const { header } = useContext(LandingProvider);
+  const { title, url, text, button } = header;
 
-  export default Header;
+  const [isDesktop, setIsDesktop] = useState(false);
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    if (window.innerWidth > 769) {
+      setIsDesktop(true);
+      setIsMobile(false);
+    } else {
+      setIsMobile(true);
+      setIsDesktop(false);
+    }
+  }, []);
+
+  return (
+    <header className="header" id="mapHeader">
+      <Fade
+        left={isDesktop}
+        bottom={isMobile}
+        duration={1000}
+        delay={700}
+        distance="40px"
+      >
+        <div className="head">
+          <h1 className="title-header">
+            {title}
+            <br />
+            CHANGING THE WORLD
+          </h1>
+          <div>
+            <div>
+              <p className="message">{text}</p>
+              <Fade
+                right={isDesktop}
+                bottom={isMobile}
+                duration={1800}
+                delay={1000}
+              >
+                <Link
+                  to="contact"
+                  smooth
+                  duration={1000}
+                  className="contact"
+                  href={url}
+                >
+                  {button || "Contact us"}
+                </Link>
+              </Fade>
+            </div>
+          </div>
+        </div>
+      </Fade>
+    </header>
+  );
+};
+
+export default Header;
