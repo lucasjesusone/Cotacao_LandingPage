c18e99d498572c31bf5eb0726b6b68be3e839059
lucasjesusone <lucasjesusone@gmail.com>
2021-02-20 00:31:02 -0400

    #93 UseState and UseEffect

--- src/components/main/Main.jsx
+++ src/components/main/Main.jsx
@@ -1,38 +1,53 @@
-import React, { useContext, Fragment} from 'react';
-import '../main/main.css'
-import LandingContext from '../../context/context';
+import React, { useContext, Fragment, useState, useEffect } from "react";
+import Fade from "react-reveal/Fade";
+import "../main/main.css";
+import LandingContext from "../../context/context";
 
 const Main = () => {
+  const { main } = useContext(LandingContext);
+  const { mainMap } = main;
 
-const { main } = useContext(LandingContext)
-const { mainMap } = main
-      
-  
-  
-    return (
-      <main id='main'>
-          <div> 
-            <div className='services'>
-                {mainMap &&
-                       mainMap.map(function(item) {
-                
-                        const {icon, title, text, id} = item
-                      return (
-                  <Fragment key={id}>
-                    <div className='service-one'> 
-                        <p className='service-icon'>{icon}</p>
-                        <p className='service-title'>{title || 'sla'}</p>
-                        <p>{text}</p>
+  const [isDesktop, setIsDesktop] = useState(false);
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    if (window.innerWidth > 768) {
+      setIsDesktop(true);
+      setIsMobile(false);
+    } else {
+      setIsMobile(true);
+      setIsDesktop(false);
+    }
+  }, []);
+
+  return (
+    <main id="main">
+      <div>
+        <div className="services">
+          {mainMap &&
+            mainMap.map(function (item) {
+              const { icon, title, text, id } = item;
+              return (
+                <Fragment key={id}>
+                  <Fade
+                    bottom={isDesktop}
+                    left={isMobile}
+                    duration={1000}
+                    delay={600}
+                  >
+                    <div className="service-one">
+                      <p className="service-icon">{icon}</p>
+                      <p className="service-title">{title || "sla"}</p>
+                      <p>{text}</p>
                     </div>
-                 </Fragment>
-                )
-              })}
-            
-          </div>
+                  </Fade>
+                </Fragment>
+              );
+            })}
+        </div>
       </div>
-</main>
-   );
- }
-
+    </main>
+  );
+};
 
 export default Main;
