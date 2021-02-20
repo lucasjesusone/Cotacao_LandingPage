23d1b3821cfe46561313c1527c1af654c31e8c39
lucasjesusone <lucasjesusone@gmail.com>
2021-02-20 00:31:35 -0400

    #97 UseState and UseEffect

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,22 +1,39 @@
-import React, { useContext } from 'react';
-import LandingProvider from '../../context/context'
-import '../contact/contact.css'
-import Form from '../form/Form'
-
+import React, { useContext, useState, useEffect } from "react";
+import Fade from "react-reveal/Fade";
+import LandingProvider from "../../context/context";
+import "../contact/contact.css";
+import Form from "../form/Form";
 
 const Contact = () => {
+  const { contact } = useContext(LandingProvider);
+  const { titleContact } = contact;
 
-const { contact } = useContext(LandingProvider)  
-const { titleContact } = contact
+  const [isDesktop, setIsDesktop] = useState(false);
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    if (window.innerWidth > 768) {
+      setIsDesktop(true);
+      setIsMobile(false);
+    } else {
+      setIsMobile(true);
+      setIsDesktop(false);
+    }
+  }, []);
 
   return (
-    <div id='contact'>
-        <section >
-            <h2 >{titleContact || 'olá'}</h2>
-            <Form />
-        </section>
+    <div id="contact">
+      <section>
+        <Fade left={isDesktop} bottom={isMobile} duration={1000} delay={500}>
+          <h2>{titleContact || "olá"}</h2>
+        </Fade>
+
+        <Fade right={isDesktop} bottom={isMobile} duration={1500} delay={500}>
+          <Form />
+        </Fade>
+      </section>
     </div>
-   );
-}
+  );
+};
 
 export default Contact;
