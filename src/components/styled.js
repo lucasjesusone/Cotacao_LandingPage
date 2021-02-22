6319b2a9207a17c9e71f2af6677bffa537383d95
lucasjesusone <lucasjesusone@gmail.com>
2021-02-21 22:30:57 -0400

    #110 Changing App.jsx css to styled component

---
+++ src/components/styled.js
@@ -0,0 +1,16 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+     background-color: #FFF;
+     width: 100%;
+     max-width: 1500px;
+  
+
+    @media screen and (min-width: 100px) and (max-width: 800px)  {
+    
+          width: 100%;
+          max-width: 700px;
+          overflow-x: hidden;
+
+    }
+`;
