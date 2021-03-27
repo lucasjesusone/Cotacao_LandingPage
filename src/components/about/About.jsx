026788e089ec462cc8df5b26a6af26084dafd9a5
lucasjesusone <lucasjesusone@gmail.com>
2021-03-26 22:25:04 -0400

    Atualização delay time

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -24,10 +24,10 @@ const About = () => {
     <div id="about">
       <div className="about-us">
         <section className="intro">
-          <Fade right={isDesktop} left={isMobile} duration={1000} delay={600}>
+          <Fade right={isDesktop} left={isMobile} duration={1000} delay={550}>
             <h2 className="title">{textabout || "Lorem"}</h2>
           </Fade>
-          <Fade right={isDesktop} bottom={isMobile} duration={1000} delay={600}>
+          <Fade right={isDesktop} bottom={isMobile} duration={1000} delay={550}>
             <div>
               <p className="textMessage">{message || "Lorem"}</p>
             </div>
